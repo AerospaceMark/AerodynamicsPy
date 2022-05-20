b8d43152890a18f653fbbccd95d73d20968e99b4
Mark C. Anderson <anderson.mark.az@gmail.com>
2022-05-20 08:10:07 -0600

    Angles and unit vectors calculated

--- aerodynamicspy/airfoils/Airfoil.py
+++ aerodynamicspy/airfoils/Airfoil.py
@@ -15,8 +15,18 @@ class Airfoil:
         # Calculate the center points
         self.Centers = np.transpose([(x[0:-1] + x[1:]) / 2, (z[0:-1] + z[1:]) / 2])
 
-        print(self.Coords)
+        # Calculate the length of each panel
+        self.Lengths = np.sqrt((x[0:-1] - x[1:])**2 + (z[0:-1] - z[1:])**2)
 
+        # Calculate the sines and cosines for the panels
+        angles = pd.DataFrame
+        angles.cos_theta_i = (x[1:] - x[0:-1]) / self.Lengths
+        angles.sin_theta_i = (z[1:] - z[0:-1]) / self.Lengths
+        self.Angles = angles
+
+        # Calculate the unit normals and tangents
+        self.Unit_Normals = np.transpose([-self.Angles.sin_theta_i, self.Angles.cos_theta_i])
+        self.Unit_Tangents = np.transpose([self.Angles.cos_theta_i, self.Angles.sin_theta_i])
 
     def plot(self, details = False):
 
@@ -26,11 +36,28 @@ class Airfoil:
                 label = "Airfoil")
 
         if details:
+
+            # Plot the center points
             ax.plot(self.Centers[:,0],self.Centers[:,1],
                     label = "Center Points",
                     color = "red",
                     marker = "o",
                     linestyle = "")
+
+            # Plot the unit normals and tangents
+            size_factor = 0.05
+            centers_x = np.transpose([self.Centers[:,0], self.Centers[:,0]])
+            centers_z = np.transpose([self.Centers[:,1], self.Centers[:,1]])
+            normals_x = np.transpose([np.zeros(len(self.Unit_Normals)), self.Unit_Normals[:,0]]) * size_factor + centers_x
+            normals_z = np.transpose([np.zeros(len(self.Unit_Normals)), self.Unit_Normals[:,1]]) * size_factor + centers_z
+
+            tangents_x = np.transpose([np.zeros(len(self.Unit_Tangents)), self.Unit_Tangents[:,0]]) * size_factor + centers_x
+            tangents_z = np.transpose([np.zeros(len(self.Unit_Tangents)), self.Unit_Tangents[:,1]]) * size_factor + centers_z
+
+            for i in range(len(normals_x)):
+                ax.plot(normals_x[i,:],normals_z[i,:], color = "green")
+                ax.plot(tangents_x[i,:],tangents_z[i,:], color = "purple")
+            
 
         ax.set_aspect('equal')
         ax.set_title(self.Name)
